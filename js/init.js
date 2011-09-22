04da0fd7e73f0bc08a7b0d936df7a1e57b153e34
Michael Pereira <michael@monkeycycle.org>
2011-09-22 14:39:27 -0400

    More vars for rendering.

--- js/init.js
+++ js/init.js
@@ -7,15 +7,35 @@ $(document).ready(function(){
 		var chartTitleColor = $('#titlecolor').val();
 
 		var chartType 		= $('#charttype').val();
-		var chartWidth	 	= $('#chartwidth').val();
-		var chartHeight	 	= $('#chartheight').val();
-		var chartColor	 	= $('#chartcolor').val();
+		if(chartType == 'h' ){ 
+								var thisChartType = 'bhs'; 
+		}
+		else{ 
+								var thisChartType = 'bvs'; 
+		}
 
-		var RenderedOutput = chartTitle + chartTitleSize + chartTitleColor + chartType + chartWidth + chartHeight + chartColor;
+		var chartWidth	 	= $('#chartwidth').val();
+
+		var chartHeight	 	= $('#chartheight').val();
+
+		var chartColor	 	= $('#chartcolor').val();
+		var chartBarColor	= $('#bar-color').val();
+
+		var chartData		= ;
+		var chartLabels		= ;
+
+		var RenderedOutput 	= chartTitle + chartTitleSize + chartTitleColor + chartType + chartWidth + chartHeight + chartColor;
+
+
+		var chartPreviewSRC	= 'http://chart.googleapis.com/chart?cht=' + thisChartType + '&chs=' + chartWidth + 'x' + chartHeight + '&chdlp=r&chco=' + chartBarColor + '&chxt=x%2Cy&chxr&chbh=a%2C3&chd=t:' + chartData + '&chxl=' + chartLabels + '&chxs&chxtc&chma=|150&chtt=' + chartTitle + '&chts=' + chartTitleColor + '%2C' + chartTitleSize ;
+
+		$("#chartPreview").attr("src",chartPreviewSRC);
 
 		$("#codeOutput").html(RenderedOutput);
 
 	});
+
+
 
 });
 
