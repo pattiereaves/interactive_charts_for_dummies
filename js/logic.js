8ab7910d3e4b0b57a20d5f1b31dd6aec69e2564f
Kevin Schaul <kevin.schaul@gmail.com>
2011-09-22 14:38:30 -0400

    Strip non-numeral characters from column two, error messages

--- js/logic.js
+++ js/logic.js
@@ -1,12 +1,24 @@
 var formatted_data = new Object;
+var error_messages = new Object;
 
 function convertStringToJSON(string) {
+  var numError = false;
   string_rows = string.split(/\n/);
   for (index in string_rows) {
     split_row = string_rows[index].split(/\t/);
-    stripped_number = split_row[1].replace(/%/,'');
-    formatted_data[index] = [split_row[0], stripped_number];
-    console.log(formatted_data);
+    if (index === 0) {
+      formatted_data['header-1'] = split_row[0];
+      formatted_data['header-2'] = split_row[1];
+    } else {
+      stripped_number = split_row[1].replace(/%|\$|,|#/g,'');
+      re = /^[0-9]*$/;
+      if (!re.test(stripped_number)) {
+        alert('Row ' + index + ', column two contains an invalid character. Only numbers are allowed in column two. Please fix and resubmit.');
+      } else {
+        formatted_data[index] = [split_row[0], stripped_number];
+        console.log(formatted_data);
+      }
+    }
   }
 }
 
