f2a30c309c1c4eb26e3fdb2ef27957b730a10658
Michael Pereira <michael@monkeycycle.org>
2011-09-22 15:16:36 -0400

    Working on getting colour values, with or without a 'u'.

--- js/init.js
+++ js/init.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
 		var chartTitle 		= $('#title').val();
 		var chartTitleSize 	= $('#titlesize').val();
-		var chartTitleColor = $('#titlecolor').val();
+		var chartTitleColor = $('#title-color').val();
 
 		var chartType 		= $('#charttype').val();
 		if(chartType == 'h' ){ 
@@ -18,13 +18,14 @@ $(document).ready(function(){
 
 		var chartHeight	 	= $('#chartheight').val();
 
-		var chartColor	 	= $('#chartcolor').val();
+		var chartCanvasColor = $('#canvas-color').val();
+		var chartColor	 	= $('#chart-color').val();
 		var chartBarColor	= $('#bar-color').val();
 
 		var chartData		= '';
 		var chartLabels		= '';
 
-		var RenderedOutput 	= chartTitle + chartTitleSize + chartTitleColor + chartType + chartWidth + chartHeight + chartColor;
+		var RenderedOutput 	= 'thisChartType: ' + thisChartType + ' chartTitle: ' + chartTitle + ' chartTitleSize:' + chartTitleSize + ' chartTitleColor:' + chartTitleColor + ' chartWidth:' + chartWidth + ' chartHeight:' + chartHeight + ' chartColor:' + chartColor + ' chartCanvasColor : ' + chartCanvasColor + ' chartBarColor: ' + chartBarColor;
 
 
 		var chartPreviewSRC	= 'http://chart.googleapis.com/chart?cht=' + thisChartType + '&chs=' + chartWidth + 'x' + chartHeight + '&chdlp=r&chco=' + chartBarColor + '&chxt=x%2Cy&chxr&chbh=a%2C3&chd=t:' + chartData + '&chxl=' + chartLabels + '&chxs&chxtc&chma=|150&chtt=' + chartTitle + '&chts=' + chartTitleColor + '%2C' + chartTitleSize ;
