1f79a2dfe448f9c5665d552fbc5a83fe1739f381
Kevin Schaul <kevin.schaul@gmail.com>
2011-09-22 14:58:54 -0400

    Fixed numbering, error messages

--- js/logic.js
+++ js/logic.js
@@ -6,17 +6,17 @@ function convertStringToJSON(string) {
   string_rows = string.split(/\n/);
   for (index in string_rows) {
     split_row = string_rows[index].split(/\t/);
-    if (index === 0) {
+    if (index == 0) {
       formatted_data['header-1'] = split_row[0];
       formatted_data['header-2'] = split_row[1];
     } else {
       stripped_number = split_row[1].replace(/%|\$|,|#/g,'');
       re = /^[0-9]*$/;
       if (!re.test(stripped_number)) {
-        alert('Row ' + index + ', column two contains an invalid character. Only numbers are allowed in column two. Please fix and resubmit.');
+        var adjusted_index = parseInt(index) + 1;
+        $('#data-error').html('Row ' + adjusted_index + ', column two contains an invalid character. Only numbers are allowed in column two. Please fix and resubmit.');
       } else {
         formatted_data[index] = [split_row[0], stripped_number];
-        console.log(formatted_data);
       }
     }
   }
