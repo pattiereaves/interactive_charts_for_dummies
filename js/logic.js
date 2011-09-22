ba2e56fb2077f91dbc20847076bce61f57a2a5f8
Pattie Reaves <pattie@pzunk.net>
2011-09-22 14:19:31 -0400

    Adds logic function to data field on index

--- js/logic.js
+++ js/logic.js
@@ -1,14 +1,5 @@
 var formatted_data = new Object;
 
-$(document).ready(function() {
-  var input_text;
-  $('#example-form').submit(function() {
-    input_text = document.getElementById('example-form').getElementsByClassName('example-ta')[0].value;
-    convertStringToJSON(input_text);
-    return false;
-  });
-});
-
 function convertStringToJSON(string) {
   string_rows = string.split(/\n/);
   for (index in string_rows) {
@@ -17,4 +8,10 @@ function convertStringToJSON(string) {
     formatted_data[index] = [split_row[0], stripped_number];
     console.log(formatted_data);
   }
+}
+
+function getData() {
+	var data_value;
+	data_value = $('#data').val();
+	convertStringToJSON(data_value);
 }
