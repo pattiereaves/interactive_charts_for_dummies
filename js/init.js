a363690414c35e2bd3488fa61eae411f6c2749a9
Michael Pereira <michael@monkeycycle.org>
2011-09-22 13:15:44 -0400

    Adding variables from form to init.js.

--- js/init.js
+++ js/init.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
 
-  $("#render").click(function(){
+	$("#render").click(function(){
+
+		var chartTitle 		= $('#title').val();
+		var chartTitleSize 	= $('#titlesize').val();
+		var chartTitleColor = $('#titlecolor').val();
+
+		var chartType 		= $('#charttype').val();
+		var chartWidth	 	= $('#chartwidth').val();
+		var chartHeight	 	= $('#chartheight').val();
+		var chartColor	 	= $('#chartcolor').val();
 
 
 
 
+		$("#rendered").html(RenderedOutput);
 
-
-    $("#rendered").html(RenderedOutput);
-
-  });
+	});
 
 });
 
